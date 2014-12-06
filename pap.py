303aaea21b15115b4bc66ccb8c335b7431730e06
Ent <Ent@Ents-MacBook-Pro.local>
2014-12-06 23:02:48 +0000

    Input Stream.

--- pap.py
+++ pap.py
@@ -1,6 +1,16 @@
+def get_input_stream(inputs):
+    if not inputs:
+        return (('Java', 'Hadoop', 'RDBMS', 'Prolog', 'Lisp', 'Pascal',),
+                ('Java', 'Java', 'RDBMS', 'Prolog', 'Prolog',),
+                ('Java', 'Hadoop', 'RDBMS', 'Prolog', 'Lisp', 'Pascal'),
+               )    
+    else:
+        raise Exception('Input situation not addressed.')
+
+
 def start(input_files, mapper, sorter, reducer, output):
     input_stream = get_input_stream(input_files)
-    mapped_data = initiate_mapper(input_stream, mapper)
+    mapped_data = map_data(input_stream, mapper)
     sorted_data = sort_data(mapped_data, sorter)
     reduced_data = reduce_data(sorted_data, reducer)
     output(reduced_data)

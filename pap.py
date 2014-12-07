6a74d169963abfa9490ef4bb685320ab7346df2e
Ent <Ent@ents-mbp.home>
2014-12-07 00:35:58 +0000

    Sorter: Simple Sorter. Updated Mapper.

--- pap.py
+++ pap.py
@@ -1,6 +1,17 @@
+from collections import defaultdict
+
+
 def simple_mapper(stream):
-    from collections import defaultdict
-    return defaultdict.fromkeys(stream, 1)
+    return map(lambda key: (key, 1), stream)
+
+
+def simple_sorter(data):
+    sorted_data = defaultdict(list)
+    for data_list in data:
+        # map(lambda k,v: sorted_data[k].append(v), data_list)
+        [sorted_data[data_id].append(data_val)
+         for data_id, data_val in data_list]
+    return sorted_data
 
 
 def get_input_stream(inputs):
@@ -20,12 +31,21 @@ def map_data(streams, mapper):
     if not mapper:
         mapper = simple_mapper
 
-    mapped_data = map(mapper, stream)
+    mapped_data = list(map(mapper, streams))
 
     # for stream in streams:
     # mappend_data.append(map_process_to_core(stream, mapper))
 
     return mapped_data
+
+
+def sort_data(mdata, sorter):
+    if not sorter:
+        sorter = simple_sorter
+
+    sorted_data = sorter(mdata)
+
+    return sorted_data
 
 
 def start(input_files, mapper, sorter, reducer, output):

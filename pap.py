b7d0ea03c140877f00c1f08122b7e6f531f12070
Ent <Ent@ents-mbp.home>
2014-12-10 23:46:06 +0000

    MP: Refactoring for Greater Good.

--- pap.py
+++ pap.py
@@ -16,20 +16,45 @@ def simple_sorter(data):
     Returns a dict of key & list of tuple's second value: { key1: [1,1,...],...}
     """
     sorted_data = defaultdict(list)
+
     for data_list in data:
-        # map(lambda k,v: sorted_data[k].append(v), data_list)
-        [sorted_data[data_id].append(data_val)
-         for data_id, data_val in data_list]
+        for data_id, data_val in data_list:
+            sorted_data[data_id].append(data_val)
+
     return sorted_data
 
 
-def simple_reducer(kv_pair):
+def plex_sorter(data):
+    """
+    Accepts a singular simple_mapper output: [(<key1>, 1), (<key2>, 1)...]
+    Returns a dict of key & list of tuple's second value: { key1: [1,1,...],...}
+    """
+    sorted_data = defaultdict(list)
+
+    for data_id, data_val in data:
+        sorted_data[data_id].append(data_val)
+
+    return sorted_data
+
+
+def plex_reducer(kv_pair):
     return {kv_pair[0]: sum(kv_pair[1])}
 
 
 def simple_output(data):
     import pprint
     pprint.pprint(data)
+
+
+def map_pool(func, iter_data):
+    pool_size = multiprocessing.cpu_count() * 2
+    pool = multiprocessing.Pool(processes=pool_size)
+
+    map_pool_data = pool.map(reducer, sdata.items())
+    pool.close()
+    pool.join()
+
+    return map_pool_data
 
 
 def get_input_stream(inputs):
@@ -47,13 +72,7 @@ def map_data(streams, mapper):
     Accepts list of data streams
     Returns list of - list of tuples
     """
-    pool_size = multiprocessing.cpu_count() * 2
-    pool = multiprocessing.Pool(processes=pool_size)
-
-    mapped_data = pool.map(mapper, streams)
-    pool.close()
-    pool.join()
-
+    mapped_data = map_pool(mapper, streams)
     return mapped_data
 
 
@@ -62,28 +81,41 @@ def sort_data(mdata, sorter):
     Accepts list of - list of tuples
     Returns dict of keys & mapper's tuple value
     """
-    sorted_data = sorter(mdata)
+    pool_size = multiprocessing.cpu_count() * 2
+    pool = multiprocessing.Pool(processes=pool_size)
+    sorted_dicts = pool.map(sorter, mdata)
+    sorted_data = {}
+
+    for sorted_dict in sorted_dicts:
+        sdict_keys = sorted_dict.keys()
+        for key in sdict_keys:
+            sdata_keys = sorted_data.keys()
+            if key in sdata_keys:
+                sorted_data[key].extend(sorted_dict[key])
+            else:
+                sorted_data[key] = sorted_dict[key]
 
     return sorted_data
 
 
 def reduce_data(sdata, reducer):
-    pool_size = multiprocessing.cpu_count()*2
-    pool = multiprocessing.Pool(processes=pool_size)
     reduced_data = dict()
-    reduced_list = pool.map(reducer, sdata.items())    
+    reduced_list = map_pool(reducer, sdata)
+
     for item in reduced_list:
         reduced_data.update(item)
+
     return reduced_data
 
 
-def start(input_files=None, mapper=simple_mapper, sorter=simple_sorter,
-          reducer=simple_reducer, output=simple_output):
+def start(input_files=None, mapper=simple_mapper, sorter=plex_sorter,
+          reducer=plex_reducer, output=simple_output):
     input_stream = get_input_stream(input_files)
     mapped_data = map_data(input_stream, mapper)
     sorted_data = sort_data(mapped_data, sorter)
     reduced_data = reduce_data(sorted_data, reducer)
-    assert reduced_data == {'Java':4, 'Hadoop':2, 'RDBMS':3, 'Prolog':4, 'Lisp': 2, 'Pascal':2}
+    assert reduced_data == {
+        'Java': 4, 'Hadoop': 2, 'RDBMS': 3, 'Prolog': 4, 'Lisp': 2, 'Pascal': 2}
     output(reduced_data)
 
 
